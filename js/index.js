1d620b0cd8832620a258b7ec338b6d3971e2306c
Miguel Luis <miguelluisataideferreira68@gmail.com>
2023-09-14 19:12:37 -0300

    finalizando section modalidades

--- js/index.js
+++ js/index.js
@@ -106,44 +106,4 @@ window.addEventListener('scroll', () => {
 scrollToTopButton.addEventListener('click', scrollToTop);
 
 //  para tentar corrigir o problema do scroll top com o carrosel
-document.addEventListener("DOMContentLoaded", function () {
-  const scrollToTopButton = document.getElementById("scrollToTop");
-  const carousel = document.getElementById("carouselExampleCaptions");
 
-  // Função para verificar se o cursor está sobre o carrossel
-  function isCursorOverCarousel(event) {
-    const carouselRect = carousel.getBoundingClientRect();
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-
-    return (
-      mouseX >= carouselRect.left &&
-      mouseX <= carouselRect.right &&
-      mouseY >= carouselRect.top &&
-      mouseY <= carouselRect.bottom
-    );
-  }
-
-  // Função para alternar a classe do botão "scrollTop"
-  function toggleScrollTopButton() {
-    if (isCursorOverCarousel(event)) {
-      scrollToTopButton.classList.add("hidden");
-    } else {
-      scrollToTopButton.classList.remove("hidden");
-    }
-  }
-
-  // Adicione um ouvinte de eventos de mousemove ao documento
-  document.addEventListener("mousemove", toggleScrollTopButton);
-
-  // O botão "scrollTop" deve ser inicialmente visível
-  scrollToTopButton.classList.remove("hidden");
-});
-
-// Função para rolar suavemente para o topo da página quando o botão é clicado
-document.getElementById("scrollToTop").addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
