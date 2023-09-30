f03cc9c7cd12144f07f68e49979589e5005f7126
Miguel Luis <miguelluisataideferreira68@gmail.com>
2023-09-30 20:42:34 -0300

    Update carrinho.compras.js

--- js/carrinho.compras.js
+++ js/carrinho.compras.js
@@ -4,36 +4,53 @@ document.addEventListener("DOMContentLoaded", function () {
     const totalDisplay = document.getElementById("total");
     const checkoutButton = document.getElementById("checkout");
   
-    let cart = [];
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
     let total = 0;
   
     addToCartButtons.forEach((button) => {
-        button.addEventListener("click", () => {
-            const price = parseFloat(button.getAttribute("data-price"));
-            const plan = button.getAttribute("data-plan");
+      button.addEventListener("click", () => {
+        const price = parseFloat(button.getAttribute("data-price"));
+        const plan = button.getAttribute("data-plan");
   
-            cart.push({ plan, price });
-            total += price;
+        cart.push({ plan, price });
+        total += price;
   
-            updateCart();
-        });
+        updateCart();
+      });
     });
   
     checkoutButton.addEventListener("click", () => {
-     
-        
-        alert(`Total da compra: R$${total.toFixed(2)} redirecionando para a pagina principal`);
-        window.location = "../index.html"
+      if (cart.length === 0) {
+        alert("Seu carrinho está vazio. Adicione itens antes de finalizar a compra.");
+        return;
+      }
+  
+      // Simule uma compra
+      alert(`Total da compra: R$${total.toFixed(2)}. Redirecionando para a página principal.`);
+      cart = []; // Limpa o carrinho após a compra
+      total = 0;
+      updateCart();
+  
+      // Redireciona para a página principal
+      window.location = "../index.html";
     });
   
+    // Função para atualizar o carrinho na página
     function updateCart() {
-        cartList.innerHTML = "";
-        cart.forEach((item) => {
-            const listItem = document.createElement("li");
-            listItem.textContent = `${item.plan} - R$${item.price.toFixed(2)}`;
-            cartList.appendChild(listItem);
-        });
+      cartList.innerHTML = "";
+      cart.forEach((item) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${item.plan} - R$${item.price.toFixed(2)}`;
+        cartList.appendChild(listItem);
+      });
   
-        totalDisplay.textContent = `R$${total.toFixed(2)}`;
+      totalDisplay.textContent = `R$${total.toFixed(2)}`;
+  
+      // Salva o carrinho no LocalStorage
+      localStorage.setItem("cart", JSON.stringify(cart));
     }
+  
+    // Inicializa o carrinho na página
+    updateCart();
   });
+  
